338d0e87ccbf890936dfaf53138a288e64f3acf6
Joby Joseph <contactbackbencherteam@gmail.com>
2021-09-26 02:47:42 +0530

    Error handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-// highlight intentional extra waiting to show loading state.
-// Adding isLoading flag
-
 import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 import axios from "axios";
 
@@ -10,7 +7,7 @@ function Gitusers() {
   const gitUsers = useQuery("gitusers", async () => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     const { data } = await axios.get(
-      "https://api.github.com/search/users?q=react"
+      "https://api.github.com/search/users1?q=react"
     );
     return data;
   });
@@ -20,17 +17,23 @@ function Gitusers() {
       {gitUsers.isLoading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
-          <h1>{gitUsers.data.total_count} results found!</h1>
-          <ul>
-            {gitUsers.data.items.map((item) => (
-              <li key={item.id}>
-                <img src={item.avatar_url} width="60px" height="60px" />@
-                {item.login}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <>
+          {gitUsers.isError ? (
+            <div>{gitUsers.error.message}</div>
+          ) : (
+            <div>
+              <h1>{gitUsers.data.total_count} results found!</h1>
+              <ul>
+                {gitUsers.data.items.map((item) => (
+                  <li key={item.id}>
+                    <img src={item.avatar_url} width="60px" height="60px" />@
+                    {item.login}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </>
       )}
     </>
   );
