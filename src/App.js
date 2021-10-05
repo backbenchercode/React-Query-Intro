07527eb25bbac0a5b534e889b8d783b53f8e6caf
Joby Joseph <contactbackbencherteam@gmail.com>
2021-10-06 02:29:11 +0530

    query hook reuse

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import axios from "axios";
 
 const queryClient = new QueryClient();
 
-function Gitusers() {
-  const gitUsers = useQuery("gitusers", async () => {
+function useGitusers() {
+  return useQuery("gitusers", async () => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     const { data } = await axios.get(
       "https://api.github.com/search/users?q=react"
     );
     return data;
   });
+}
+
+function Gitusers() {
+  const gitUsers = useGitusers();
   console.log(gitUsers);
   return (
     <>
@@ -20,14 +24,21 @@ function Gitusers() {
       ) : (
         <div>
           <h1>{gitUsers.data.total_count} results found!</h1>
-          <ul>
-            {gitUsers.data.items.map((item) => (
-              <li key={item.id}>
-                <img src={item.avatar_url} width="60px" height="60px" />@
-                {item.login}
-              </li>
-            ))}
-          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+function AnotherGitusers() {
+  const gitUsers = useGitusers();
+  console.log(gitUsers);
+  return (
+    <>
+      {gitUsers.isLoading ? (
+        <h1>Another Loading...</h1>
+      ) : (
+        <div>
+          <h1>Another {gitUsers.data.total_count} results found!</h1>
         </div>
       )}
     </>
@@ -38,6 +49,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Gitusers />
+      <AnotherGitusers />
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
