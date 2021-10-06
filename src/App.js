fef743d132eafcaaf3733cb52483e6725f38ee17
Joby Joseph <contactbackbencherteam@gmail.com>
2021-10-06 11:25:40 +0530

    Dynamic Query keys

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
+import { useState } from "react";
 import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import axios from "axios";
 
 const queryClient = new QueryClient();
 
-function Gitusers() {
-  const gitUsers = useQuery("gitusers", async () => {
+function GitUserSearch() {
+  const [value, setValue] = useState("");
+
+  const gitUsers = useQuery(`user${value}`, async () => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     const { data } = await axios.get(
-      "https://api.github.com/search/users?q=react"
+      "https://api.github.com/search/users?q=" + value
     );
     return data;
   });
   console.log(gitUsers);
   return (
     <>
-      {gitUsers.isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          <h1>{gitUsers.data.total_count} results found!</h1>
-          <ul>
-            {gitUsers.data.items.map((item) => (
-              <li key={item.id}>
-                <img src={item.avatar_url} width="60px" height="60px" />@
-                {item.login}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <input type="text" onChange={(e) => setValue(e.target.value)} />
+      <h1>{gitUsers.data?.total_count} results found!</h1>
     </>
   );
 }
@@ -37,7 +27,7 @@ function Gitusers() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Gitusers />
+      <GitUserSearch />
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
